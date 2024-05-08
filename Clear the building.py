a10eab87e16ff7631627307b5efa881aa4a5bc86
Dany007Dany <ligadany0@gmail.com>
2024-05-08 13:36:57 +0200

    Inserimento prima parte del condice

---
+++ Clear the building.py
@@ -0,0 +1,48 @@
+import pygame
+from sys import exit
+
+
+pygame.init()
+
+
+#clock
+clock = pygame.time.Clock()
+
+
+#creazione finestra 
+altezza = 1000
+larghezza = 1600
+dimensioni = (larghezza, altezza)
+schermo = pygame.display.set_mode(dimensioni)
+
+#personalizzazione finestra
+colore_sfondo_1 = (0, 0, 0)
+sfondo = colore_sfondo_1
+pygame.display.set_caption("Clear the Building!")
+prog_icon = pygame.image.load("/Users/dany/Downloads/PyGame/CtB images/Icona_Stivale.png")
+
+pygame.display.set_icon(prog_icon)
+
+
+#personaggio principale
+perc_p1 = "/Users/dany/Downloads/PyGame/CtB images/P1.png"
+image_p1 = pygame.image.load(perc_p1)
+image_p1 = pygame.transform.scale(image_p1, (70, 50))
+image_p1.convert_alpha()
+
+
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #blit del personaggio
+    schermo.blit(image_p1, (0, 0))
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick
