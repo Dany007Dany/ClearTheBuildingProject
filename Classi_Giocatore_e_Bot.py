72a71a6e8f826b6ae4802acd92813233aaaeed22
Dany007Dany <ligadany0@gmail.com>
2024-05-15 13:54:58 +0200

    modifiche varie dei camp ivisivi e miglior. codice

--- Classi_Giocatore_e_Bot.py
+++ Classi_Giocatore_e_Bot.py
@@ -38,7 +38,7 @@ class Giocatore:
         
         self.vel_gioc = 5
 
-    def dis_pers(self, schermo):
+    def disegna(self, schermo):
         schermo.blit(self.immagine, self.rect)
 
     def mov (self, tastiera = None):
@@ -59,8 +59,7 @@ class Giocatore:
 
     def collisioni(self, bots, schermo, testo_you_lost_render, pos_testo_you_lost):
         for bot in bots:
-            if self.rect.colliderect(bot.campo_visivo.rect):
-                print("Collisione")
+            if self.rect.colliderect(bot.campo_visivo):
                 schermo.fill("Black")
                 schermo.blit(testo_you_lost_render, pos_testo_you_lost)
                 return True
@@ -72,28 +71,35 @@ class Giocatore:
 
 
 class Bot:
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, orientamento, x1, y1) -> None:
+        
+        
+        self.orientamento = orientamento
+        
         self.immagine_vivo = pygame.image.load("/Users/dany/Downloads/CtB images/Bot_vivo.png").convert_alpha()
         self.immagine_vivo = pygame.transform.scale(self.immagine_vivo, (80, 80))
         self.rect_vivo = self.immagine_vivo.get_rect()
         self.rect_vivo.x = x
         self.rect_vivo.y = y
+        self.rect_vivo.center = (x, y)
+       
+        self.campo_visivo = CampoVisivo(x1, y1)
+
         
-        self.campo_visivo = CampoVisivo(x, y)
         
-        self.immagine_vivo_90 = pygame.transform.rotate(self.immagine_vivo, -90)
+        self.immagine_vivo_90 = pygame.transform.rotate(self.immagine_vivo, orientamento)
         self.rect_vivo_90 = self.immagine_vivo_90.get_rect()
         self.rect_vivo_90.x = x
         self.rect_vivo_90.y = y
         self.rect_vivo_90.center = (x, y)
 
-        self.immagine_vivo_180 = pygame.transform.rotate(self.immagine_vivo, 180)
+        self.immagine_vivo_180 = pygame.transform.rotate(self.immagine_vivo, orientamento)
         self.rect_vivo_180 = self.immagine_vivo_180.get_rect()
         self.rect_vivo_180.x = x
         self.rect_vivo_180.y = y
         self.rect_vivo_180.center = (x, y)
 
-        self.immagine_vivo_270 = pygame.transform.rotate(self.immagine_vivo, 90)
+        self.immagine_vivo_270 = pygame.transform.rotate(self.immagine_vivo, orientamento)
         self.rect_vivo_270 = self.immagine_vivo_270.get_rect()
         self.rect_vivo_270.x = x
         self.rect_vivo_270.y = y
@@ -104,63 +110,48 @@ class Bot:
         self.rect_morto = self.immagine_morto.get_rect()
         self.rect_morto.x = x
         self.rect_morto.y = y
-
+        
+        self.sup0 = pygame.surface.Surface((80, 80))
+        self.sup0.fill("Red")
+        self.sup_rect0 = self.sup0.get_rect(midbottom = (self.rect_vivo.midtop))
         
 
-
-    def dis_bot_ruotato0(self, schermo):
-        schermo.blit(self.immagine_vivo, self.rect_vivo)
-    
-    def dis_bot_ruotato90(self, schermo):
-        schermo.blit(self.immagine_vivo_90, self.rect_vivo_90)
-
-    def dis_bot_ruotato_180(self, schermo):
-        schermo.blit(self.immagine_vivo_180, self.rect_vivo_180)
-    
-    def dis_bot_ruotato270(self, schermo):
-        schermo.blit(self.immagine_vivo_270, self.rect_vivo_270)
-    
-    def inserisci_campo_visivo0(self, schermo):
-        sup = pygame.surface.Surface((80, 80))
-        sup.fill("Red")
-        sup_rect = sup.get_rect(midbottom = (self.rect_vivo.midtop))
-        schermo.blit(sup, sup_rect)
-
-    def inserisci_campo_visivo90(self, schermo):
-        sup = pygame.surface.Surface((80, 80))
-        sup.fill("Red")
-        sup_rect = sup.get_rect(midleft = (self.rect_vivo_90.midright))
-        schermo.blit(sup, sup_rect)
-
-    def inserisci_campo_visivo180(self, schermo):
-        sup = pygame.surface.Surface((80, 80))
-        sup.fill("Red")
-        sup_rect = sup.get_rect(midtop = (self.rect_vivo_180.midbottom))
-        schermo.blit(sup, sup_rect)
-    
-    def inserisci_campo_visivo_ruotato270(self, schermo):
-        sup = pygame.surface.Surface((80, 80))
-        sup.fill("Red")
-        sup_rect = sup.get_rect(midright = (self.rect_vivo_270.midleft))
-        schermo.blit(sup, sup_rect)
+        self.sup90 = pygame.surface.Surface((80, 80))
+        self.sup90.fill("Red")
+        self.sup_rect90 = self.sup90.get_rect(midleft = (self.rect_vivo_90.midright))
         
-    def dis_bot_morto(self, schermo):
-        schermo.blit(self.immagine_morto, self.rect_morto)
+        self.sup180 = pygame.surface.Surface((80, 80))
+        self.sup180.fill("Red")
+        self.sup_rect180 = self.sup180.get_rect(midtop = (self.rect_vivo_180.midbottom))
+        
+        
+        self.sup270 = pygame.surface.Surface((80, 80))
+        self.sup270.fill("Red")
+        self.sup_rect270 = self.sup270.get_rect(midright = (self.rect_vivo_270.midleft))
+        
+
+    def disegna(self, schermo, orientamento, morto):
+        if orientamento == 0 and morto == False:
+            schermo.blit(self.immagine_vivo, self.rect_vivo)
+            schermo.blit(self.sup0, self.sup_rect0)
+        if orientamento == -90 and morto == False:
+            schermo.blit(self.immagine_vivo_90, self.rect_vivo_90)
+            schermo.blit(self.sup90, self.sup_rect90)
+        if orientamento == 180 and morto == False:
+            schermo.blit(self.immagine_vivo_180, self.rect_vivo_180)
+            schermo.blit(self.sup180, self.sup_rect180)
+        if orientamento == 90 and morto == False:
+            schermo.blit(self.immagine_vivo_270, self.rect_vivo_270)
+            schermo.blit(self.sup270, self.sup_rect270)
+        if morto == True:
+            schermo.blit(self.immagine_morto, self.rect_morto)
 
     
 
 class CampoVisivo:
     def __init__(self, x, y) -> None:
         self.imm = pygame.surface.Surface((80, 80))
-        self.imm.fill("Red")
-        self.rect = self.imm.get_rect(midbottom=(x, y)) 
-        self.rect.x = x
-        self.rect.y = y
+        self.rect = self.imm.get_rect(center = (x, y))
 
-    #def aggiorna(self, rect_giocatore):
-        #if self.rect.colliderect(rect_giocatore):
-            #pygame.quit()
         
-
-            
-
+        
--- Clear the building.py
+++ Clear the building.py
@@ -2,7 +2,6 @@ import pygame
 from sys import exit
 from Classi_Giocatore_e_Bot import Giocatore
 from Classi_Giocatore_e_Bot import Bot
-from Classi_Giocatore_e_Bot import CampoVisivo
 
 pygame.init()
 
@@ -18,7 +17,7 @@ dimensioni = (larghezza, altezza)
 schermo = pygame.display.set_mode(dimensioni)
 
 #personalizzazione finestra
-colore_sfondo_1 = (0, 0, 0)
+colore_sfondo_1 = ("Black")
 sfondo = colore_sfondo_1
 pygame.display.set_caption("Clear the Building!")
 prog_icon = pygame.image.load("/Users/dany/Downloads/CtB images/Icona_Stivale.png").convert_alpha()
@@ -27,12 +26,11 @@ pygame.display.set_icon(prog_icon)
 
 #creazione del giocatore
 Giocatore1 = Giocatore(x = 1500, y = 900)
-Giocatore2 = Giocatore(x = 600, y = 900)
 #creazione bot
-Bot1 = Bot(x = 300, y = 300)
-Bot2 = Bot(x = 1400, y = 600)
-Bot3 = Bot(x = 1300, y = 400)
-Bot4 = Bot(x = 600, y = 700)
+Bot1 = Bot(x = 300, y = 300, orientamento = 0, x1 = 300, y1 = 220)
+Bot2 = Bot(x = 1400, y = 600, orientamento = -90, x1 = 1480, y1 = 600)
+Bot3 = Bot(x = 1300, y = 400, orientamento = 180, x1 = 1300, y1 = 480)
+Bot4 = Bot(x = 600, y = 700, orientamento = 90, x1 = 520, y1 = 700)
 
 #YOU LOST
 testo_you_lost = "Sei stato scoperto"
@@ -44,8 +42,8 @@ you_lost_visibile = False
 
 
 
-
-
+#vettore
+objs = [Giocatore1, Bot1, Bot2, Bot3, Bot4]
 
 
 
@@ -67,25 +65,20 @@ while True:
     schermo.fill(sfondo)
     
     #blit dei bot
-    Bot1.dis_bot_ruotato0(schermo)
-    Bot2.dis_bot_ruotato90(schermo)
-    Bot3.dis_bot_ruotato_180(schermo)
-    Bot4.dis_bot_ruotato270(schermo)
+    Bot1.disegna(schermo, 0, True)
+    Bot2.disegna(schermo, -90, False)
+    Bot3.disegna(schermo, 180, False)
+    Bot4.disegna(schermo, 90, False)
     #Bot2.dis_bot_morto(schermo)
 
 
-    #blit del campo al Bot1
-    Bot1.inserisci_campo_visivo0(schermo)
-    Bot2.inserisci_campo_visivo90(schermo)
-    Bot3.inserisci_campo_visivo180(schermo)
-    Bot4.inserisci_campo_visivo_ruotato270(schermo)
     #blit del personaggio
-    Giocatore1.dis_pers(schermo)
+    Giocatore1.disegna(schermo)
     Giocatore1.collisioni([Bot1, Bot2, Bot3, Bot4], schermo, testo_you_lost_render, pos_testo_you_lost)
 
+        
     #aggiornamenti vari e eventuali 
     pygame.display.flip()
     pygame.display.update()
     clock.tick(60)
-
 
